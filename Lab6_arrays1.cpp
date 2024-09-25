058216edc6b8d883f115424cb39b89665aa1c595
Dr34mChas3r <53311619+Dr34mChas3r@users.noreply.github.com>
2024-09-25 18:37:16 +0300

    Update

--- Lab6_arrays1.cpp
+++ Lab6_arrays1.cpp
@@ -14,21 +14,21 @@ using namespace std;
 
 int main() {
 	srand(time(0));
-	int rand1 = rand() % 1001;
-	int rand2 = rand() % 1001;
-	int rand3 = rand() % 1001;
-	int rand4 = rand() % 1001;
-	int rand5 = rand() % 1001;
-	int rand6 = rand() % 1001;
-	int rand7 = rand() % 1001;
-	int rand8 = rand() % 1001;
-	int rand9 = rand() % 1001;
-	int rand10 = rand() % 1001;
+	int rand1 = rand() % 100001;
+	int rand2 = rand() % 100001;
+	int rand3 = rand() % 100001;
+	int rand4 = rand() % 100001;
+	int rand5 = rand() % 100001;
+	int rand6 = rand() % 100001;
+	int rand7 = rand() % 100001;
+	int rand8 = rand() % 100001;
+	int rand9 = rand() % 100001;
+	int rand10 = rand() % 100001;
 
-	cout << rand1 << endl << rand2 << endl << rand3 << endl << rand4 << endl << rand5 << endl << rand6 << endl << rand7 << endl << rand8 << endl << rand9 << endl << rand10 << endl;
+	cout << rand1 << " " << rand2 << " " << rand3 << " " << rand4 << " " << rand5 << " " << rand6 << " " << rand7 << " " << rand8 << " " << rand9 << " " << rand10 << " " << endl;
 	cout << "Sorted array: " << endl;
 	int myArray[]{ rand1, rand2, rand3, rand4, rand5, rand6, rand7, rand8, rand9, rand10 };
 	sort(begin(myArray), end(myArray));
-	cout << myArray[0] << endl << myArray[1] << endl << myArray[2] << endl << myArray[3] << endl << myArray[4] << endl << myArray[5] << endl << myArray[6] << endl << myArray[7] << endl << myArray[8] << endl << myArray[9] << endl;
+	cout << myArray[0] << " " << myArray[1] << " " << myArray[2] << " " << myArray[3] << " " << myArray[4] << " " << myArray[5] << " " << myArray[6] << " " << myArray[7] << " " << myArray[8] << " " << myArray[9] << " ";
 
 }
