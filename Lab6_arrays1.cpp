d82cb28760821c9612644fcaf55959d1d8a19dbf
Dr34mChas3r <53311619+Dr34mChas3r@users.noreply.github.com>
2024-09-30 22:21:22 +0300

    optimized_2

--- Lab6_arrays1.cpp
+++ Lab6_arrays1.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 int main() {
 	srand(time(0));
+
 	int rand1 = rand() % 100001;
 	int rand2 = rand() % 100001;
 	int rand3 = rand() % 100001;
@@ -24,15 +25,20 @@ int main() {
 	int rand8 = rand() % 100001;
 	int rand9 = rand() % 100001;
 	int rand10 = rand() % 100001;
+
 	int myArray[10]{ rand1, rand2, rand3, rand4, rand5, rand6, rand7, rand8, rand9, rand10 };
+
 	cout << "Random generated numbers :" << endl;
 	for (int i = 0; i < 10; i++) {
 		cout << myArray[i] << " ";
 	}
+
 	cout << endl;
+
 	cout << "Sorted array: " << endl;
 	sort(begin(myArray), end(myArray)); 
 	for (int i = 0; i < 10; i++) {
 		cout << myArray[i] << " " ;
 	}
+
 }
