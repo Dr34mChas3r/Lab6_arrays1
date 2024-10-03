fcc0d94bd1042f446430de998b9a7bdeeff9dfdd
Dr34mChas3r <53311619+Dr34mChas3r@users.noreply.github.com>
2024-10-03 22:28:40 +0300

    FINALLY! WORKING! BOMBA!

--- Lab6_arrays1.cpp
+++ Lab6_arrays1.cpp
@@ -1,5 +1,7 @@
 //example cin 10 9 8 7 6 5 4 3 2 1 
 //example cout 1 2 3 4 5 6 7 8 9 10
+// Example CIN 5 6 23 13 534 123 5304 23 32 2
+// Example COUT 2 5 6 13 23 23 32 123 534 5304
 
 #include <iostream>
 using namespace std;
@@ -28,6 +30,6 @@ int main() {
     for (i = 0; i < 10; i++) {
         cout << arr[i] << " ";
     }
-
+    
     return 0;
 }
